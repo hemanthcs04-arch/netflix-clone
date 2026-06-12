146b1374b7f50152759b4b5b6d3ddcae06692da7
hemanthcs04-arch <hemanthcs04@gmail.com>
2026-06-12 14:26:19 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/netflic-clone/',
+  base: '/netflix-clone/',
   build: {
     outDir: 'dist',
   },
